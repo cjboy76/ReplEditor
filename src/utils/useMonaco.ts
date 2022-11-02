1433cb7a2bc5d12bc7a87d414a96d5bf0273d61e
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2022-11-02 23:19:50 +0800

    fix: editor create options type
    useMonaco.ts useMonaco args type

--- src/utils/useMonaco.ts
+++ src/utils/useMonaco.ts
@@ -1,8 +1,13 @@
 import { editor } from 'monaco-editor';
 import darktheme from 'theme-vitesse/themes/vitesse-dark.json';
 import lightTheme from 'theme-vitesse/themes/vitesse-light.json';
+import type { editor as Editor } from 'monaco-editor';
 
-function useMonaco(element: HTMLElement, language: string, args: any) {
+function useMonaco(
+  element: HTMLElement,
+  language: string,
+  args: Editor.IStandaloneEditorConstructionOptions
+) {
   // @ts-ignore
   editor.defineTheme('vitesse-dark', darktheme);
   // @ts-ignore
